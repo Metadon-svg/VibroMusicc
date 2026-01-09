aa36ed32bbc55cf6740931e7437197a67e875522
Metadon-svg <krisevicu@gmail.com>
2026-01-09 20:22:30 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     // НОВЫЕ ЗАВИСИМОСТИ:
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.media3:media3-session:1.2.1")
+    implementation("androidx.media3:media3-ui:1.2.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
